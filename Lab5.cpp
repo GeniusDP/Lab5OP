98bd573a2c7e7c29a6ddd9e8a27a42e9ba0e51a2
GeniusDP <zaranikbz@gmail.com>
2021-05-04 13:24:34 +0300

    Добавили структуру PointOnMap.

--- Lab5.cpp
+++ Lab5.cpp
@@ -1,8 +1,24 @@
 ﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct PointOnMap{
+    double Lat, Long;
+    double x, y;
+    string type;
+    string subType;
+    string name;
+    string adress;
+    void converse() {
+        x = 1;//formulas
+        y = 1;
+    }
+};
 
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
     return 0;
 }
