a4e188034c1e9b83e15e8b35f126ad8f79a9e0fc
Vladyslav2020 <vmskl2020@gmail.com>
2021-05-04 15:06:45 +0300

    Added finding points at circle.

--- Lab5.cpp
+++ Lab5.cpp
@@ -22,13 +22,11 @@ int main()
         cin >> x1 >> y1;
         tree.removePoint(tree.getRoot(), { x1, y1 });
         vector< Point > result;
-        Rectangle rect = { { 2, 2 }, { 8, 8 } };
-        tree.findPointsInRect(tree.getRoot(), rect, result);
+        tree.findPointsInCircle(tree.getRoot(), { 5, 5 }, 5, result);
         cout << "Size: " << result.size() << endl;
         for (int i = 0; i < result.size(); i++)
             cout << result[i].x << " " << result[i].y << endl;
         
     }
-    
     return 0;
 }
