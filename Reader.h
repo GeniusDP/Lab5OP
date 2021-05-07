4d7b1c567a6091da468fce82f46b660cea223f47
GeniusDP <zaranikbz@gmail.com>
2021-05-07 22:33:41 +0300

    При считывании разделителем мантиссы и антье числа была ",", а в стандартных функциях С - точка. Это приводило к считыванию ТОЛЬКО целой части числа.

--- Reader.h
+++ Reader.h
@@ -9,14 +9,16 @@ PointOnMap split(string s) {
 	PointOnMap result;
 	int left = 0;
 	vector<string> splittedStrings;
+	for (int i = 0; i < s.size(); i++)
+		if (s[i] == ',')s[i] = '.';
 	while (left < s.size()) {
 		int right = left;
 		while (s[right] != ';')right++;
 		splittedStrings.push_back(s.substr(left, right - left));
 		left = right + 1;
 	}
-	result.Lat = stof(splittedStrings[0]);
-	result.Long = stof(splittedStrings[1]);
+	result.Lat = atof(splittedStrings[0].c_str());
+	result.Long = atof(splittedStrings[1].c_str());
 	result.type = splittedStrings[2];
 	result.subType = splittedStrings[3];
 	result.name = splittedStrings[4];
